66ab798adcea9e3ddee7e362c7bc81b846ec7000
WestonLu <westonlu@foxmail.com>
2021-02-25 16:20:28 +0800

    利用 docker-entrypoint-initdb.d 初始化数据库, 利用healthcheck 确保 mysql 数据库已启动

--- mysql/mysql-init-files/setup.sh
+++ mysql/mysql-init-files/setup.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 echo `service mysql status`
 echo '1 Service Start.....'
-mkdir /home/mysql
-chmod 755 /home/mysql
 service mysql start
 sleep 3
 echo `service mysql status`
 echo '2.Create table'
-mysql< /Data/schema.sql
+mysql< /mysql/schema.sql
 sleep 3
 echo '3. Create user'
-mysql< /Data/changeUser.sql
-echo '4 stop service.....'
-sleep 3
-service mysql stop
+mysql< /mysql/changeUser.sql
 sleep 3
 echo `service mysql status`
-echo '5 ok'
+echo '4 ok'
